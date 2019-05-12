019fd51fa12f7edb027122756d8017ab107b0299
Hodaya1234 <ko.hodaya@gmail.com>
2019-05-12 10:50:44 +0300

    tiny change in net, need to check other models

--- dense_net.py
+++ dense_net.py
@@ -8,7 +8,7 @@ class DenseNet(nn.Module):
         self.linear1 = nn.Linear(D_in, H1)
         self.linear2 = nn.Linear(H1, H2)
         self.linear3 = nn.Linear(H2, D_out)
-        self.relu = nn.ReLU()
+        self.relu = nn.LeakyReLU()
         self.drop = nn.Dropout(p=p)
         self.batchnorm = nn.BatchNorm1d(D_in)
 
@@ -36,10 +36,10 @@ def weights_init(m):
         torch.nn.init.xavier_uniform(m.weight.data)
 
 
-def init_weights(net):
-    for modul in net._modules.values():
-        if type(modul) == nn.Linear:
-            nn.init.normal_(modul.weight, mean=0, std=1)
-            prev_size = modul.weight.shape[1]
-            modul.weight.data = modul.weight * torch.sqrt(torch.tensor(2, dtype=torch.float32)/prev_size)
-    return net
+# def init_weights(net):
+#     for modul in net._modules.values():
+#         if type(modul) == nn.Linear:
+#             nn.init.normal_(modul.weight, mean=0, std=1)
+#             prev_size = modul.weight.shape[1]
+#             modul.weight.data = modul.weight * torch.sqrt(torch.tensor(2, dtype=torch.float32)/prev_size)
+#     return net
