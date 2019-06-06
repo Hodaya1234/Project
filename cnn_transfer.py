85b637940ee8eb7ad80d6079b6d5debf0c9f3845
Hodaya1234 <ko.hodaya@gmail.com>
2019-06-06 15:44:20 +0300

    working on transfer cnn

--- cnn_transfer.py
+++ cnn_transfer.py
@@ -12,7 +12,12 @@ import time
 import os
 import copy
 from data_set import DataSet
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn import svm
+import sys
 
+use_cuda = torch.cuda.is_available()
 
 def load_data(raw_path, frames):
     raw = sio.loadmat(raw_path)
@@ -133,7 +138,7 @@ def fine_tune_cnn():
 
 def transform(x, y, n_frames):
     pad = int((224 - 100) / 2)
-    data = np.zeros([len(y), 3, 224, 224, n_frames])
+    data = np.zeros([x.shape[0], 3, 224, 224, n_frames])
     for i in range(3):
         data[:, i, pad:224 - pad, pad:224 - pad, :] = x
     return data
@@ -144,43 +149,102 @@ def data_through_cnn(model, x_data, n_frames, single_frame_length):
     for idx, x in enumerate(x_data):
         all_frame_vec = np.empty([n_frames, single_frame_length])
         for i in range(n_frames):
-            vec = model(torch.from_numpy(x[np.newaxis, :, :, :, i]))
+            input = torch.from_numpy(x[np.newaxis, :, :, :, i])
+            if use_cuda:
+                input = input.cuda()
+            vec = model(input)
             all_frame_vec[i, :] = vec
         new_x[idx, :] = all_frame_vec.flatten()
     return new_x
 
 
-frames = np.arange(27,68)
-all_x, all_y = load_data('Data/02.12/a/clean.mat', frames)
-all_x[all_x == 0] = np.nan
+def create_cnn_data(raw_data_path):
+    single_length = 512
+    model_conv = models.resnet18(pretrained=True).double()
+    for param in model_conv.parameters():
+        param.requires_grad = False
+    model_conv.fc = Identity()
+    if use_cuda:
+        model_conv = model_conv.cuda()
+    frames = np.arange(27,68)
+    all_x, all_y = load_data(raw_data_path, frames)
+    n_data = len(all_y)
+    train_x_sets = []
+    val_x_sets = []
+    train_y_sets = []
+    val_y_sets = []
+    for val_index in range(n_data):
+        print(val_index)
+        train_indices = np.delete(np.arange(n_data), val_index)
+        train_y = all_y[train_indices]
+        val_y = all_y[val_index]
 
-val_indices = [0,7]
-train_indices = [i for i in range(14) if i not in val_indices]
+        all_x[all_x == 0] = np.nan
 
-train_y = all_y[train_indices]
-val_y = all_y[val_indices]
+        val_x = all_x[val_index, :, :, :]
+        val_x = val_x[np.newaxis, :, :, :]
+        train_x = all_x[train_indices, :, :, :]
 
-val_x = all_x[val_indices,:,:,:]
-train_x = all_x[train_indices,:,:,:]
-m = np.nanmean(train_x, axis=0)
-s = np.nanstd(train_x, axis=0)
-train_x = np.divide(np.subtract(train_x, m), s)
-val_x = np.divide(np.subtract(val_x, m), s)
+        m = np.nanmean(train_x, axis=0)
+        s = np.nanstd(train_x, axis=0)
+        train_x = np.divide(np.subtract(train_x, m), s)
+        val_x = np.divide(np.subtract(val_x, m), s)
 
-train_x = np.nan_to_num(train_x)
-val_x = np.nan_to_num(val_x)
+        train_x = np.nan_to_num(train_x)
+        val_x = np.nan_to_num(val_x)
 
-train_x = transform(train_x, train_y, len(frames))
-val_x = transform(val_x, val_y, len(frames))
+        train_x = transform(train_x, train_y, len(frames))
+        val_x = transform(val_x, val_y, len(frames))
 
-# go over all examples: for each frame get a vector output from the cnn, concatenate them. insert to NN
-single_length = 512
-model_conv = models.resnet18(pretrained=True).double()
-for param in model_conv.parameters():
-    param.requires_grad = False
-model_conv.fc = Identity()
+        new_train_x = data_through_cnn(model_conv, train_x, len(frames), single_length)
+        new_val_x = data_through_cnn(model_conv, val_x, len(frames), single_length)
 
-new_train_x = data_through_cnn(model_conv, train_x, len(frames), single_length)
-new_val_x = data_through_cnn(model_conv, val_x, len(frames), single_length)
+        train_x_sets.append(new_train_x)
+        train_y_sets.append(train_y)
+        val_x_sets.append(new_val_x)
+        val_y_sets.append(val_y)
+    return np.asarray(train_x_sets), np.asarray(train_y_sets), np.asarray(val_x_sets), np.asarray(val_y_sets)
 
-print('done')
+
+def plot_pca(x, y):
+    fig = plt.figure(1, figsize=(4, 3))
+    plt.clf()
+    ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+
+    plt.cla()
+    pca = PCA(n_components=3)
+    pca.fit(x)
+    X = pca.transform(x)
+
+    # Reorder the labels to have colors matching the cluster results
+    ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=y, cmap=plt.cm.nipy_spectral,
+               edgecolor='k')
+
+    plt.show()
+
+
+def main():
+    args = sys.argv
+    train_x, train_y, val_x, val_y = create_cnn_data(args[1])
+    np.savez(args[2], train_x=train_x, train_y=train_y, val_x=val_x, val_y=val_y)
+
+
+new_data = np.load('new_data_a.npz')
+train_x = new_data['arr_0']
+train_y = new_data['arr_1']
+val_x = new_data['arr_2']
+val_y = new_data['arr_3']
+corrects = 0
+for i in range(len(train_x)):
+    clf = svm.SVC(gamma='scale')
+    x = train_x[i,:,:]
+    y = train_y[i,:]
+    x_val = val_x[i,:,:]
+    m = np.mean(x, axis=0)
+    s = np.std(x, axis=0)
+    x = np.divide(np.subtract(x, m), s)
+    x_val = (x_val - m) / s
+    clf.fit(x, y)
+    y_hat = clf.predict(x_val)
+    corrects += (y_hat == val_y[i])
+print(corrects / len(train_x))
